e8c99cdbac8cc59f93dea5841a72459521fcba92
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-04-21 13:48:42 +0200

    Fixed SCSS problems

--- egonoid.com/src/components/Bio/Bio.tsx
+++ egonoid.com/src/components/Bio/Bio.tsx
@@ -9,8 +9,6 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 
-// import { rhythm } from '../../utils/typography';
-
 import styles from './Bio.module.scss';
 
 const Bio = () => {
--- egonoid.com/src/declarations.d.ts
+++ egonoid.com/src/declarations.d.ts
@@ -1,2 +1,7 @@
 declare module 'typography-theme-wordpress-2016';
 declare const __PATH_PREFIX__: string;
+declare const graphql: (query: TemplateStringsArray) => void;
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export = content;
+}
