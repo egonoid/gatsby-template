0152c5871d0315ef734ebe5fa661182e1cfa4638
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-04-21 12:02:28 +0200

    Added unit test for Bio component

--- egonoid.com/src/components/Bio/Bio.test.tsx
+++ egonoid.com/src/components/Bio/Bio.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
+import * as Gatsby from 'gatsby';
 import { Bio } from '.';
 
 describe('<Bio />', () => {
@@ -7,6 +8,29 @@ describe('<Bio />', () => {
 
   it('renders correctly', () => {
     // Arrange
+    const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery');
+    useStaticQuery.mockImplementation(() => ({
+      avatar: {
+        childImageSharp: {
+          fixed: {
+            width: 300,
+            height: 300,
+            src: '',
+            srcSet: '',
+          },
+        },
+      },
+      site: {
+        siteMetadata: {
+          author: 'Florian',
+          description: 'My description',
+          title: 'My Title',
+          social: {
+            twitter: '',
+          },
+        },
+      },
+    }));
 
     // Act
     const container = render(<Bio />);
