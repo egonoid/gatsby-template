f6d5f62f5de5385eaff9ef6318489567ecf9a718
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-04-21 13:42:54 +0200

    Configured SCSS support

--- egonoid.com/src/components/Bio/Bio.tsx
+++ egonoid.com/src/components/Bio/Bio.tsx
@@ -9,7 +9,9 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 
-import { rhythm } from '../../utils/typography';
+// import { rhythm } from '../../utils/typography';
+
+import styles from './Bio.module.scss';
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -37,24 +39,11 @@ const Bio = () => {
 
   const { author, social } = data.site.siteMetadata;
   return (
-    <div
-      style={{
-        display: `flex`,
-        marginBottom: rhythm(2.5),
-      }}
-    >
+    <div className={styles.container}>
       <Image
         fixed={data.avatar.childImageSharp.fixed}
         alt={author.name}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
+        className={styles.image__container}
       />
       <p>
         Written by <strong>{author.name}</strong> {author.summary}
