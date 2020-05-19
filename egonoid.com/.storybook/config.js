c0cbcededf9ced41cd9f1e0c9305f5547095f3bb
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-05-19 11:19:22 +0200

    Added default stories [#172197670]

--- egonoid.com/.storybook/config.js
+++ egonoid.com/.storybook/config.js
@@ -2,7 +2,7 @@ import { configure } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.js$/), module);
+configure(require.context('../src', true, /\.stories\.tsx$/), module);
 
 // Gatsby's Link overrides:
 // Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
