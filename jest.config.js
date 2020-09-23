a4944d3868165194f03cc0a1f86dbd7c29b8de3d
Andreas Hoffmann <hi@andreashoffmann.me>
2020-09-23 08:34:46 +0200

    Refactored Jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,15 @@
 module.exports = {
   transform: {
-    // "^.+\\.tsx?$": "ts-jest",
-    '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
+    '^.+\\.[jt]sx?$': '<rootDir>/jest-configs/jest-preprocess.js',
   },
   testRegex: '(/__tests__/.*\\.([tj]sx?)|(\\.|/)(test|spec))\\.([tj]sx?)$',
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/file-mock.js',
+      '<rootDir>/jest-configs/__mocks__/file-mock.js',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['node_modules', '.cache'],
+  testPathIgnorePatterns: ['node_modules', '.cache', '.public'],
   transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
   globals: {
     __PATH_PREFIX__: '',
@@ -19,5 +18,5 @@ module.exports = {
     },
   },
   testURL: 'http://localhost',
-  setupFiles: ['<rootDir>/loadershim.js'],
+  setupFiles: ['<rootDir>/jest-configs/loadershim.js'],
 };
